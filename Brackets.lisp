0982ff2f09a95ca669077c7510de17e9c5fce009
James Hamilton <mrjameshamilton@gmail.com>
2017-12-25 10:16:15 +0100

    Add "balanced brackets" test (Lisp)

---
+++ Brackets.lisp
@@ -0,0 +1,30 @@
+(defun is_balanced (s) (is_balanced_list (coerce s 'list)))
+
+(defun is_balanced_list (l) (is_balanced_r l NIL))
+
+(defun is_balanced_r (l stack)
+    (cond
+        ((and (eq NIL l) (eq NIL stack)) t)
+        ((is_open (car l)) (is_balanced_r (cdr l) (cons (car l) stack)))
+        ((is_pair (car stack) (car l)) (is_balanced_r (cdr l) (cdr stack)))
+    )
+)
+
+(defun is_open (c)
+    (not (eq NIL (member c '("(" "{" "[") :test #'string=)))
+)
+
+(defun is_pair (a b)
+    (cond
+        ((and (string= a "(") (string= b ")")) t)
+        ((and (string= a "{") (string= b "}")) t)
+        ((and (string= a "[") (string= b "]")) t)
+    )
+)
+
+(format t "T ~a~%" (is_balanced ""))
+(format t "T ~a~%" (is_balanced "{[]}"))
+(format t "T ~a~%" (is_balanced "{[()()]}"))
+(format t "NIL ~a~%" (is_balanced "([)()]"))
+(format t "NIL ~a~%" (is_balanced "]"))
+(format t "NIL ~a~%" (is_balanced "{"))
